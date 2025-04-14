de95f4b536946531e1d790f732f0c61d99beda6d
yzemlyak <yzemlyak@gmail.com>
2025-04-14 17:33:51 +0000

    Added initial directions

--- qr.rb
+++ qr.rb
@@ -1,2 +1,12 @@
 # Write your solution here!
-pp "hello world"
+# pp "hello world"
+require "rqrcode"
+
+# Use the RQRCode::QRCode class to encode some text
+qrcode = RQRCode::QRCode.new("https://en.wikipedia.org/wiki/QR_code")
+
+# Use the .as_png method to create a 500 pixels by 500 pixels image
+png = qrcode.as_png({ :size => 500 })
+
+# Write the image data to a file
+IO.binwrite("sometext.png", png.to_s)
